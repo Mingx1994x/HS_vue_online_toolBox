4603b02deea3deb84018518c3514bf7bf3937f89
Mingx1994x <mggxmingx@gmail.com>
2025-09-01 00:53:52 +0800

    feat: 完成 todoList 頁面 手機版 header 區塊與 input 區塊

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -34,7 +34,7 @@ watch(logoutState, () => {
 // 使用者狀態
 const userLoginState = computed(() => userState.value?.status ?? false)
 
-const userNickname = computed(() => (userState.value ? `${userState.value.nickname}'s'` : ''))
+const userNickname = computed(() => (userState.value ? `${userState.value.nickname}` : ''))
 
 // getTodo
 const { data: todos, isLoading, isFetching } = useTodo({ enabled: userLoginState })
@@ -50,7 +50,7 @@ watchEffect(() => {
 })
 
 //  addTodo
-const newTodo = ref('備份專案資料')
+const newTodo = ref('')
 const { mutate: addTodo } = useAddTodo()
 
 // removeTodo
@@ -83,39 +83,63 @@ const handleEditTodo = (id, content) => {
 }
 </script>
 <template>
-  <div class="about">
-    <h1>{{ userNickname }} TodoList</h1>
-    <button type="button" @click="signOut">登出</button>
-  </div>
-  <div>
-    <input type="text" v-model="newTodo" />
-    <button @click="addTodo(newTodo)">新增</button>
-  </div>
-  {{ todos }}
-  <ul v-if="todoList.length !== 0">
-    <template v-for="todo in todoList" :key="todo.id">
-      <li>
-        <template v-if="!editModeTodos[todo.id]?.isEditing">
-          <div class="d-flex content-start center items-center">
-            <button type="button" v-if="todo.status" @click="toggleTodo(todo.id)">勾</button>
-            <button type="button" v-else @click="toggleTodo(todo.id)">口</button>
-            <p v-if="isPending">取得資料中...</p>
-            <p v-else>{{ todo.content }}</p>
-            <button type="button" @click="handleEditMode(todo.id, todo.content)">編輯</button>
-            <button type="button" @click="removeTodo(todo.id)">刪除</button>
-          </div>
+  <div class="mx-auto px-3 pt-4">
+    <div class="w-full md:w-1/2 lg:w-1/3 mx-auto px-5">
+      <div class="flex justify-between items-center mb-4">
+        <img src="/logo_lg.svg" alt="logo" style="width: 242.51px" />
+        <div class="flex">
+          <h2 class="hidden md:block">{{ userNickname }}的代辦</h2>
+          <a role="button" @click="signOut">登出</a>
+        </div>
+      </div>
+      <div class="relative">
+        <input
+          type="text"
+          id="register_nickname"
+          class="bg-neutral-50 text-neutral-500 border-none rounded-[10px] focus:ring-neutral-500 focus:border-neutral-500 block w-full py-3 ps-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="新增待辦事項"
+          v-model.trim="newTodo"
+        />
+        <button
+          class="btn-square btn-neutral rounded-[10px] absolute bottom-0 right-0 -translate-[4px]"
+          @click="addTodo(newTodo)"
+        >
+          <span class="material-symbols-outlined add-icon"> add </span>
+        </button>
+      </div>
+      <!-- {{ todos }} -->
+      <ul v-if="todoList.length !== 0">
+        <template v-for="todo in todoList" :key="todo.id">
+          <li>
+            <template v-if="!editModeTodos[todo.id]?.isEditing">
+              <div class="d-flex content-start center items-center">
+                <button type="button" v-if="todo.status" @click="toggleTodo(todo.id)">勾</button>
+                <button type="button" v-else @click="toggleTodo(todo.id)">口</button>
+                <p v-if="isPending">取得資料中...</p>
+                <p v-else>{{ todo.content }}</p>
+                <button type="button" @click="handleEditMode(todo.id, todo.content)">編輯</button>
+                <button type="button" @click="removeTodo(todo.id)">刪除</button>
+              </div>
+            </template>
+            <template v-else>
+              <input type="text" v-model="editModeTodos[todo.id].content" />
+              <button type="button" @click="handleEditMode(todo.id)">取消</button>
+              <button
+                type="button"
+                @click="handleEditTodo(todo.id, editModeTodos[todo.id].content)"
+              >
+                確認編輯
+              </button>
+            </template>
+          </li>
         </template>
-        <template v-else>
-          <input type="text" v-model="editModeTodos[todo.id].content" />
-          <button type="button" @click="handleEditMode(todo.id)">取消</button>
-          <button type="button" @click="handleEditTodo(todo.id, editModeTodos[todo.id].content)">
-            確認編輯
-          </button>
-        </template>
-      </li>
-    </template>
-  </ul>
-  <div v-else>沒有資料</div>
+      </ul>
+      <div v-else>
+        <p>目前尚無待辦事項</p>
+        <img src="/empty 1.svg" alt="目前尚無待辦事項" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
